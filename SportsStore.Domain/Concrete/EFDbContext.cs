6cbc7fa3d83cb90929c51d346a277c6a922beea4
Mateusz Kozik <mateuszkozik100@gmail.com>
2021-03-15 13:39:25 +0100

    Create product repository

--- SportsStore.Domain/Concrete/EFDbContext.cs
+++ SportsStore.Domain/Concrete/EFDbContext.cs
@@ -8,7 +8,7 @@ using SportsStore.Domain.Entities;
 
 namespace SportsStore.Domain.Concrete
 {
-    class EFDbContext : DbContext 
+    public class EFDbContext : DbContext 
     {
         public DbSet<Product> Products { get; set; }
     }
