df024f004a6e082725ec47b6efdc8015fbc0c0cc
Mateusz Kozik <mateuszkozik100@gmail.com>
2021-03-15 15:49:05 +0100

    Reafactor test, view and controller to use ProductListViewModel

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.WebUI.Controllers
 {
@@ -21,10 +22,20 @@ namespace SportsStore.WebUI.Controllers
         // return list of all products
         public ViewResult List(int page = 1)
         {
-            return View(repository.Products
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
                 .OrderBy(p => p.ProductID)
-                .Skip((page-1)*PageSize)
-                .Take(PageSize));
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+            return View(model);
         }
     }
 }
