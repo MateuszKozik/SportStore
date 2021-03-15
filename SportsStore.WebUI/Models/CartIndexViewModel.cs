2e566a91bb87d6518b7692872dcfaee512e52bc0
Mateusz Kozik <mateuszkozik100@gmail.com>
2021-03-15 20:30:40 +0100

    Create cart view

--- SportsStore.WebUI/Models/CartIndexViewModel.cs
+++ SportsStore.WebUI/Models/CartIndexViewModel.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using SportsStore.Domain.Entities;
+
 
 namespace SportsStore.WebUI.Models
 {
     public class CartIndexViewModel
     {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
     }
 }
