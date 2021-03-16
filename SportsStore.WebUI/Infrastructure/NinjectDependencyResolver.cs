7740ecf9306acc9aabf441ce0131bd0e491535c3
Mateusz Kozik <mateuszkozik100@gmail.com>
2021-03-16 10:51:22 +0100

    Create class and interface for email order processor

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -6,6 +6,7 @@ using Ninject;
 using Moq;
 using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Abstract;
+using System.Configuration;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -32,6 +33,13 @@ namespace SportsStore.WebUI.Infrastructure
         private void AddBindings()
         {
             kernel.Bind<IProductRepository>().To<EFProductRepository>();
+
+            EmailSettings emailSettings = new EmailSettings
+            {
+                WriteAsFile = bool.Parse(ConfigurationManager.AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+
+            kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>().WithConstructorArgument("settings", emailSettings);
         }
     }
 
